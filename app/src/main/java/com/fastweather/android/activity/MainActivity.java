e304fc2cf630950934805e5e314c4982217298fd
heixiaohuai <1305558920@qq.com>
2019-04-29 14:59:02 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/fastweather/android/activity/MainActivity.java
+++ app/src/main/java/com/fastweather/android/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.fastweather.android;
+package com.fastweather.android.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.fastweather.android.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/fastweather/android/pojo/County.java
@@ -0,0 +1,37 @@
+package com.fastweather.android.pojo;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 13055 on 2019/4/29.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
