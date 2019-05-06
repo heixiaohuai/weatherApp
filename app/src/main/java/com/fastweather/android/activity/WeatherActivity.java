66c641dcda0c7faf20c0ab19c5cd8d7e00a1413c
heixiaohuai <1305558920@qq.com>
2019-05-06 10:05:48 +0800

    防止代码遗失，上传日期为2019年5月6号早上10:05

--- app/src/main/java/com/fastweather/android/activity/WeatherActivity.java
+++ app/src/main/java/com/fastweather/android/activity/WeatherActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
